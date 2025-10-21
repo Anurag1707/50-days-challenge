fe8f3090c1bbb58a5097f1a59f02a3b94f1734e8
Anurag1707 <Anuragjhariya1707@gmail.com>
2025-10-21 09:09:28 +0530

    day 2

--- code2.c
+++ code2.c
@@ -1,13 +1,25 @@
-#include<stdio.h>
-int main(){
-    int arr[5]={1,2,3,4,5};
-    for(int i=0;i<5/2;i++){
-        int temp=arr[i];
-        arr[i]=arr[5-i-1];
-        arr[5-i-1] = temp;
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, temp;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    // Reverse in place
+    for(i = 0; i < n / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
     }
-    for(int i=0;i<5;i++){
-       printf("%d",arr[i]);
-    }
-   
+
+    printf("Reversed array:\n");
+    for(i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
 }
